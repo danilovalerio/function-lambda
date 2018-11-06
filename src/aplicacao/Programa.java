a2d23b6c5b2ac4040232a127288c1be64a48e19f
danilovalerio <danilo_vs@hotmail.com>
2018-11-06 12:55:44 -0200

    Expressão lambda declarada

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -7,6 +7,7 @@ package aplicacao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -22,8 +23,11 @@ public class Programa {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//implementação via método NÃO estático
-		List<String> nomes = list.stream().map(Product::naoStaticUpperCaseName).collect(Collectors.toList());
+		//implementação via expressão lambda declarada
+		//func igual a p que recebe o getNome do produto em Upper
+		Function<Product,String> func = p -> p.getNome().toUpperCase();
+		
+		List<String> nomes = list.stream().map(func).collect(Collectors.toList());
 		
 		nomes.forEach(System.out::println);
 
